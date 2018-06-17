2202a1843eae89bdf152532db3a27101f3836307
Jeff P <jeff@siccr.com>
2018-06-17 09:43:16 -0600

    separate boot and setup scripts

---
+++ jenkins/hygieia_configure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BUILD_WS=/home/ubuntu
+HYCI=${BUILD_WS}/hyci
+PUPPETPARTS=${BUILD_WS}/puppetparts
+HYGIEIA=${BUILD_WS}/hygieia
+
+#chown -R ubuntu:ubuntu $BUILD_WS && sudo -H -u ubuntu bash -c "sh $HYCI/jenkins/hygieia_setup_and_run.sh $BUILD_WS"
+
+echo 'Configuration Completed.'
